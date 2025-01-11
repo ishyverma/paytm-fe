11c87a1785cea599d35dc09c8286b6efa8152b41
ishyverma <ishyamver@gmail.com>
2025-01-11 19:01:02 +0530

    completed frontend

--- http/src/index.ts
+++ http/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
--- http/src/lib/middleware.ts
+++ http/src/lib/middleware.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config";
-
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const header = req.headers["authorization"]
     if (!header || !header.startsWith("Bearer")) {
@@ -12,7 +10,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     }
     const token = header.split(" ")[1];
     try {
-        const userInfo = jwt.verify(token, JWT_SECRET) as { userId: number }
+        const userInfo = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: number }
         req.userId = userInfo.userId;
         next();
     } catch (e) {
