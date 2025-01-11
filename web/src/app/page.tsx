7c446e4f0956bdb41e39a67281802a2d402eac18
ishyverma <ishyamver@gmail.com>
2025-01-11 21:28:07 +0530

    completed frontend

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 import { Toaster } from 'sonner'
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    window.location.href = "/signup"
+  }, [])
   return <div className='bg-black'>
     <Toaster position='bottom-right' />
   </div>
